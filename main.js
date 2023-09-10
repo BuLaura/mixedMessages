23557f51aa708c01c6852520eaae526b476ccacb
Laura Buchwald <laura.buchwald@outlook.com>
2023-09-10 12:53:06 -0400

    fix bug & add test function

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const FortuneCookie = {
+const fortuneCookie = {
     time: ['Yestrday', 'Day after tomorrow', 'Never', 'Always', 'Next year', 'Your entire life', 'Today', 'Sometimes', 'Last year', 'Next month', 'Next Week', 'Next summer', 'Next winter'],
     wisdom: ['count back words until you see', 'remember to smile for', 'is going to pay of so remember', 'forget to to regret', 'do not hold onto', 'look back at', 'realize how much you love', 'sit in silence with', 'go out of your way for'],
     subject: ['your family.', 'your friends.', 'your neighbor.', 'love.', 'time.', 'patience.', 'hope.', 'happiness.', 'money.', 'people.', 'your career.', 'the past.', 'the future.', 'your life.'],
@@ -7,11 +7,19 @@ const FortuneCookie = {
     },
     get newLuck() {
         let mixedMessage = [];
-        mixedMessage.push(this.time[Math.floor(Math.random() * this.time.length - 1)]);
-        mixedMessage.push(this.wisdom[Math.floor(Math.random() * this.wisdom.length - 1)]);
-        mixedMessage.push(this.subject[Math.floor(Math.random() * this.subject.length -1)]);
-        return `Your fortune cookie reads: ${mixedMessage.join(" ")} and your lucky number is: ${this.luckyNumber()}`;
+        mixedMessage.push(this.time[Math.floor(Math.random() * (this.time.length - 1))]);
+        mixedMessage.push(this.wisdom[Math.floor(Math.random() * (this.wisdom.length - 1))]);
+        mixedMessage.push(this.subject[Math.floor(Math.random() * (this.subject.length -1))]);
+        return `Your fortune cookie reads: ${mixedMessage.join(" ")} & your lucky number is: ${this.luckyNumber()} !`;
     }
 }
 
-console.log(FortuneCookie.newLuck);
+// test function
+
+const test = (fortuneCookie) => { 
+    for (let i = 0; i < 100; i++) {
+        console.log(fortuneCookie.newLuck);
+    }
+}
+
+test(fortuneCookie);
